562447eff3e56128a797395782ca5be250bf4f40
Idylio Viel <idylio@mandelbrot.com.br>
2015-08-05 14:41:54 -0300

    Tradução para o inglês

--- src/ofApp.h
+++ src/ofApp.h
@@ -7,6 +7,41 @@
 class ofApp : public ofBaseApp{
 
 private:
+    
+    std::map<string, string> ptStrings;
+    std::map<string, string> enStrings;
+    std::map<string, string> currentStrings;
+    int currentLocale;
+    const static string SUPPORT_BUTTON_NAME;
+    const static float MAX_STRENGTH_AROUND_PIXEL;
+    const static string CHANGE_LOCALE_BUTTON_NAME;
+    const static string ENGLISH_LABEL;
+    const static string PORTUGUESE_LABEL;
+    const static int LOCALE_ENGLISH = 0;
+    const static int LOCALE_PORTUGUESE = 1;
+    ofxUILabelButton* changeLocaleButton;
+    
+    ofxUILabel* titleLabel;
+    ofxUIDropDownList* pickCameraLabel;
+    ofxUILabel* cameraWidthLabel;
+    ofxUILabel* cameraHeightLabel;
+    ofxUILabel* imageRotationLabel;
+    ofxUILabel* maxSpeedLabel;
+    ofxUILabel* maxHeightLabel;
+    ofxUILabel* minutesLabel;
+    ofxUILabelButton* clearButton;
+    
+    ofxUILabel* credits1Label;
+    ofxUILabel* credits2Label;
+    ofxUILabel* credits3Label;
+    ofxUILabel* credits4Label;
+    ofxUILabel* credits5Label;
+    
+    ofxUILabelButton* saveButton;
+    ofxUILabelButton* cancelButton;
+    
+    ofxUILabel* supportLabel;
+    
     ofxUICanvas *gui;
     ofxUICanvas *cameraPanel;
     ofxUICanvas *imagePanel;
@@ -34,20 +69,6 @@ private:
     float lastTimeImageWasSaved;
     int intervalToSaveImage;
 
-    const static string CAMERA_WIDTH_LABEL;
-    const static string CAMERA_HEIGHT_LABEL;
-    const static string MAX_SPEED_LABEL;
-    const static string MAX_HEIGHT_LABEL;
-    const static string SAVE_IMAGE_LABEL;
-    const static string SAVE_LABEL;
-    const static string CANCEL_LABEL;
-    const static string RESET_IMAGE_LABEL;
-    const static string SUPPORT_BUTTON_NAME;
-
-    const static string ZERO_DEGREES_LABEL;
-    const static string NINETY_DEGREES_LABEL;
-    const static string ONE_HUNDRED_EIGHTY_DEGREES_LABEL;
-    const static string TWO_HUNDRED_SEVENTY_DEGREES_LABEL;
 
     ofxUISpacer* titleSpacer;
     ofxUIDropDownList* cameraList;
